ee53278d0eb6f1e8ea1f2144c05b9a12c8a4b172
Ivan Semenov <ivsemenovdev@gmail.com>
2023-05-05 16:19:48 +0300

    feat: add styles to TodoList component

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,25 +2,116 @@ import React from "react";
 
 function TodoList() {
   return (
-    <div>
-      <ul>
-        <li>
-          <span>item1</span>
-          <button>Удалить</button>
-          <button>Редактировать</button>
-          <button>Изменить статус</button>
+    <div className="list-group container p-0">
+      <h2 className="ps-3 pb-1 m-0">Список задач:</h2>
+      <ul className="p-0">
+        <li className="list-group-item row d-flex m-0">
+          <div className="col-md-10 col-12 p-2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+            consectetur inventore ipsum provident, vel voluptatum. Aliquid
+            ducimus maxime vitae voluptates?
+          </div>
+          <div className="col-md-2 d-flex justify-content-around align-items-center">
+            <i className="bi bi-trash3 p-2"></i>
+            <i className="bi bi-pencil p-2"></i>
+            <i className="bi bi-check p-2"></i>
+          </div>
         </li>
-        <li>
-          <span>item2</span>
-          <button>Удалить</button>
-          <button>Редактировать</button>
-          <button>Изменить статус</button>
+        <li className="list-group-item row d-flex m-0">
+          <div className="col-md-10 col-12 p-2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+            consectetur inventore ipsum provident, vel voluptatum. Aliquid
+            ducimus maxime vitae voluptates?
+          </div>
+          <div className="col-md-2 d-flex justify-content-around align-items-center">
+            <i className="bi bi-trash3 p-2"></i>
+            <i className="bi bi-pencil p-2"></i>
+            <i className="bi bi-check p-2"></i>
+          </div>
         </li>
-        <li>
-          <span>item3</span>
-          <button>Удалить</button>
-          <button>Редактировать</button>
-          <button>Изменить статус</button>
+        <li className="list-group-item row d-flex m-0">
+          <div className="col-md-10 col-12 p-2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+            consectetur inventore ipsum provident, vel voluptatum. Aliquid
+            ducimus maxime vitae voluptates?
+          </div>
+          <div className="col-md-2 d-flex justify-content-around align-items-center">
+            <i className="bi bi-trash3 p-2"></i>
+            <i className="bi bi-pencil p-2"></i>
+            <i className="bi bi-check p-2"></i>
+          </div>
+        </li>
+        <li className="list-group-item row d-flex m-0">
+          <div className="col-md-10 col-12 p-2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+            consectetur inventore ipsum provident, vel voluptatum. Aliquid
+            ducimus maxime vitae voluptates?
+          </div>
+          <div className="col-md-2 d-flex justify-content-around align-items-center">
+            <i className="bi bi-trash3 p-2"></i>
+            <i className="bi bi-pencil p-2"></i>
+            <i className="bi bi-check p-2"></i>
+          </div>
+        </li>
+        <li className="list-group-item row d-flex m-0">
+          <div className="col-md-10 col-12 p-2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+            consectetur inventore ipsum provident, vel voluptatum. Aliquid
+            ducimus maxime vitae voluptates?
+          </div>
+          <div className="col-md-2 d-flex justify-content-around align-items-center">
+            <i className="bi bi-trash3 p-2"></i>
+            <i className="bi bi-pencil p-2"></i>
+            <i className="bi bi-check p-2"></i>
+          </div>
+        </li>
+        <li className="list-group-item row d-flex m-0">
+          <div className="col-md-10 col-12 p-2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+            consectetur inventore ipsum provident, vel voluptatum. Aliquid
+            ducimus maxime vitae voluptates?
+          </div>
+          <div className="col-md-2 d-flex justify-content-around align-items-center">
+            <i className="bi bi-trash3 p-2"></i>
+            <i className="bi bi-pencil p-2"></i>
+            <i className="bi bi-check p-2"></i>
+          </div>
+        </li>
+        <li className="list-group-item row d-flex m-0">
+          <div className="col-md-10 col-12 p-2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+            consectetur inventore ipsum provident, vel voluptatum. Aliquid
+            ducimus maxime vitae voluptates?
+          </div>
+          <div className="col-md-2 d-flex justify-content-around align-items-center">
+            <i className="bi bi-trash3 p-2"></i>
+            <i className="bi bi-pencil p-2"></i>
+            <i className="bi bi-check p-2"></i>
+          </div>
+        </li>
+        <li className="list-group-item row d-flex m-0">
+          <div className="col-md-10 col-12 p-2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+            consectetur inventore ipsum provident, vel voluptatum. Aliquid
+            ducimus maxime vitae voluptates?
+          </div>
+          <div className="col-md-2 d-flex justify-content-around align-items-center">
+            <i className="bi bi-trash3 p-2"></i>
+            <i className="bi bi-pencil p-2"></i>
+            <i className="bi bi-check p-2"></i>
+          </div>
+        </li>
+        <li className="list-group-item row d-flex m-0">
+          <div className="col-md-10 col-12 p-2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+            consectetur inventore ipsum provident, vel voluptatum. Aliquid
+            ducimus maxime vitae voluptates?
+          </div>
+          <div className="col-md-2 d-flex justify-content-around align-items-center">
+            <i className="bi bi-trash3 p-2"></i>
+            <i className="bi bi-pencil p-2"></i>
+            <i className="bi bi-check p-2"></i>
+          </div>
         </li>
       </ul>
     </div>
