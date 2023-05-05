5e7a2afa87755c0c6b6e6bc396ed2dfa67eb8940
Ivan Semenov <ivsemenovdev@gmail.com>
2023-05-05 13:42:49 +0300

    feat: add styles to AddTodo component

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -2,9 +2,27 @@ import React from "react";
 
 function AddTodo() {
   return (
-    <div>
-      <input type="text" />
-      <button>Добавить</button>
+    <div className="container py-5 px-4">
+      <form>
+        <div className="row">
+          <div className="col-md-10 col-12 p-2">
+            <input
+              type="text"
+              className="form-control w-100"
+              placeholder="Введите задачу"
+              autoFocus="true"
+            ></input>
+          </div>
+          <div className="col-md-2 col p-2">
+            <button
+              type="submit"
+              className="btn btn-dark btn-wrapper-end w-100"
+            >
+              Добавить
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
