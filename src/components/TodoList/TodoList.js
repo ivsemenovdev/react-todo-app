d67c6dcdf238b6f3c26bbcb807773cf9843f189c
Ivan Semenov <ivsemenovdev@gmail.com>
2023-05-06 11:45:43 +0300

    feat: add functionality for editing a task

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 
 function TodoList({tasks, setTask}) {
 
-  console.log(tasks);
+  // console.log(tasks);
+
+  const [edit, setEdit] = useState(null);
+  const [value, setValue] = useState("");
 
   function deleteTask(id) {
     const newTasks = [...tasks].filter((task) => task.id !== id);
@@ -20,29 +23,69 @@ function TodoList({tasks, setTask}) {
     setTask(newTasks);
   }
 
+  function editTask(id, title) {
+    setEdit(id);
+    setValue(title);
+  }
+
+  function saveTask(id) {
+    let newTask = [...tasks].map(task => {
+      if (task.id === id) {
+        task.title = value;
+      }
+      return task
+    })
+    setTask(newTask);
+    setEdit(null);
+  }
+
   return (
-    <div>
-      <div className="list-group container p-0">
-        <h2 className="ps-3 pb-2 m-0">Список задач:</h2>
-        <ul className="p-0">
-          {
-            tasks.map((task) => {
-              return (
-                <li key={task.id} className="list-group-item row d-flex m-0">
-                  <div className="col-md-10 col-12 p-2">
-                    {task.title}
-                  </div>
-                  <div className="col-md-2 d-flex justify-content-around align-items-center">
-                    <i role="button" title="Удалить задачу" className="bi bi-trash3 p-2" onClick={() => deleteTask(task.id)}></i>
-                    <i className="bi bi-pencil p-2"></i>
-                    <i role="button" title="Закрыть/Открыть задачу" className="bi bi-check p-2" onClick={() => statusTask(task.id)}></i>
-                  </div>
-                </li>
-              )
-            })
-          }
-        </ul>
-      </div>
+    <div className="list-group container p-0">
+      <h2 className="ps-3 pb-2 m-0">Список задач:</h2>
+      <ul className="p-0">
+        {
+          tasks.map((task) => {
+            return (
+              <li key={task.id} className="list-group-item row d-flex m-0 ">
+                {
+                  edit === task.id ?
+                    <div className="col-md-10 col-12 p-2">
+                      <input className="form-control" type="text" value={value}
+                             onChange={(e) => setValue(e.target.value)}/>
+                    </div>
+                    :
+                    <div className="col-md-10 col-12 p-2 text-wrap">
+                      {task.title}
+                    </div>
+                }
+
+                {
+                  edit === task.id ?
+                    <div className="col-md-2 col p-2">
+                      <button
+                        type="submit"
+                        title="Сохранить измения"
+                        className="btn btn-dark btn-wrapper-end w-100"
+                        onClick={() => saveTask(task.id)}
+                      >
+                        Сохранить
+                      </button>
+                    </div>
+                    :
+                    <div className="col-md-2 d-flex justify-content-around align-items-center">
+                      <i role="button" title="Удалить задачу" className="bi bi-trash3 p-2"
+                         onClick={() => deleteTask(task.id)}></i>
+                      <i role="button" title="Редактировать задачу" className="bi bi-pencil p-2"
+                         onClick={() => editTask(task.id, task.title)}></i>
+                      <i role="button" title="Закрыть/Открыть задачу" className="bi bi-check p-2"
+                         onClick={() => statusTask(task.id)}></i>
+                    </div>
+                }
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
